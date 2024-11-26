d9cd241460e947ab4da0eab89181389528e472c3
AdonayXX <emdiazdu@est.utn.ac.cr>
2024-11-26 12:45:07 -0600

    Actualiza el origen permitido en la API de películas para restringir el acceso a https://proyectoutn.shop

--- controller/api_peliculas.php
+++ controller/api_peliculas.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://proyectoutn.shop");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json");
